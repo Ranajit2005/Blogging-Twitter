96221998dc86ef2d095e0fb99cf24d8b81bb1f2c
Ranajit2005 <18ranajit2005@gmail.com>
2025-02-21 18:20:50 +0530

    fix: enhance post state logging during post creation

--- components/post/From.tsx
+++ components/post/From.tsx
@@ -60,7 +60,13 @@ const From = ({placeholder, user, setPosts, postId, isComment } : Props) => {
           console.log("Check new post",newPost)
 
           // setPosts((prev)=> [newPost,...prev] );
-          setPosts((prev) => (Array.isArray(prev) ? [newPost, ...prev] : [newPost]));
+          // setPosts((prev) => (Array.isArray(prev) ? [newPost, ...prev] : [newPost]));
+          setPosts((prev) => {
+            console.log("Previous posts state:", prev);
+            console.log("Type :",typeof(prev))
+            return Array.isArray(prev) ? [newPost, ...prev] : [newPost];
+          });
+          
           
           setImage("");
         }
