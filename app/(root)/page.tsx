625c583e5f70aeb90fdff0e530efd519c2d1dbe6
Ranajit2005 <18ranajit2005@gmail.com>
2025-02-25 16:09:17 +0530

    data fetching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,19 @@
 
 import { getPosts } from "@/actions/post.action";
 import GetPosts from "@/components/post/GetPosts"
+// import { useRouter } from "next/navigation";
+
 
 export default async function Home() {
 
+  // const { router } = useRouter();
+
   let loading = false;
   const posts = await getPosts(loading, 10);
+
+  console.log("1st time ost value is : ",posts)
+
+  if(!posts) return null;
 
   return <GetPosts posts={posts} loading={loading} />;
   
