a8ebf1314a9eae4e0ee8ac981988b37b8d812abb
Ranajit2005 <18ranajit2005@gmail.com>
2025-03-08 01:18:10 +0530

    ui correction

--- app/(root)/profile/[userId]/page.tsx
+++ app/(root)/profile/[userId]/page.tsx
@@ -1,26 +1,30 @@
-import { getUsersById } from '@/actions/user.action';
-import Header from '@/components/common/Header'
-import ProfileBio from '@/components/users/ProfileBio';
-import ProfileHero from '@/components/users/ProfileHero';
-import React from 'react'
+import { getUsersById } from "@/actions/user.action";
+import Header from "@/components/common/Header";
+import ProfileBio from "@/components/users/ProfileBio";
+import ProfileHero from "@/components/users/ProfileHero";
+import { CircleUserRound } from "lucide-react";
+import React from "react";
 
 const ProfileUser = async ({
-    params,
-}:{
-    params: Promise<{userId:string}>
+  params,
+}: {
+  params: Promise<{ userId: string }>;
 }) => {
-
-    const { userId } = await params;
-    const getUserDetails = await getUsersById(userId);
-
+  const { userId } = await params;
+  const getUserDetails = await getUsersById(userId);
+  console.log("->", getUserDetails?.user);
 
   return (
     <>
-      <Header title=""/>
-      <ProfileHero user = {JSON.parse(JSON.stringify(getUserDetails?.user))}/>
-      <ProfileBio user = {JSON.parse(JSON.stringify(getUserDetails?.user))  }/>
-    </>
-  )
-}
+      <div className="flex justify-center items-center  pl-3 sm:pl-5">
+        <CircleUserRound />
+        <Header title={`${getUserDetails?.user?.username}`} />
+      </div>
 
-export default ProfileUser
+      <ProfileHero user={JSON.parse(JSON.stringify(getUserDetails?.user))} />
+      <ProfileBio user={JSON.parse(JSON.stringify(getUserDetails?.user))} />
+    </>
+  );
+};
+
+export default ProfileUser;
